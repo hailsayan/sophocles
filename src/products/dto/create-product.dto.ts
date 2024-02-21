1709fbc8076e47ec6e18d177543e73c7f6829204
Psyon <sayan.h2020@gmail.com>
2024-02-21 12:13:15 +0330

    create validationPipe

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,5 +1,20 @@
+import {
+  IsNotEmpty,
+  IsNumber,
+  IsOptional,
+  IsString,
+  MinLength,
+} from 'class-validator';
+
 export class CreateProductDto {
+  @IsString()
+  @IsNotEmpty()
   readonly title: string;
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(10)
   readonly description: string;
+  @IsNumber()
+  @IsOptional()
   readonly price: number;
 }
