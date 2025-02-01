2cbbd07062015980931fb9ea4b30ba12baf131e8
Psyon <psyon.dev@gmail.com>
2025-02-01 20:47:04 +0330

    feat: Add HTTP error

---
+++ pkg/constant/response.go
@@ -0,0 +1,5 @@
+package constant
+
+const (
+	ResponseSuccessMessage = "success"
+)
--- pkg/utils/ginutils/response.go
+++ pkg/utils/ginutils/response.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/hailsayan/gogol/pkg/constant"
 	"github.com/hailsayan/gogol/pkg/dto"
 )
 
