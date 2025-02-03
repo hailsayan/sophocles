103e7b1c8a277628337136d03e591190a8ae8c8c
Psyon <psyon.dev@gmail.com>
2025-02-03 11:44:48 +0330

    feat: Add PostgreSQL database

--- pkg/database/kafka.go
+++ pkg/database/kafka.go
@@ -1,6 +1,11 @@
 package database
 
-import "github.com/IBM/sarama"
+import (
+	"log"
+	"time"
+
+	"github.com/IBM/sarama"
+)
 
 type KafkaProducerOptions struct {
 	SaramaConfig   *sarama.Config
@@ -17,4 +22,58 @@ type KafkaConsumerOptions struct {
 	Brokers       []string
 	ConsumerGroup string
 	InitialOffset int64
+}
+
+func NewKafkaSyncProducer(opt *KafkaProducerOptions) *sarama.SyncProducer {
+	config := opt.SaramaConfig
+	if config == nil {
+		config = sarama.NewConfig()
+	}
+	config.Producer.RequiredAcks = opt.Acks
+	config.Producer.Retry.Max = opt.RetryMax
+	config.Producer.Return.Successes = opt.ReturnSuccess
+	config.Producer.Compression = sarama.CompressionGZIP
+	config.Producer.Flush.Frequency = time.Duration(opt.FlushFrequency) * time.Millisecond
+
+	producer, err := sarama.NewSyncProducer(opt.Brokers, config)
+	if err != nil {
+		log.Fatalf("failed to create kafka producer: %v", err)
+	}
+
+	return &producer
+}
+
+func NewKafkaAsyncProducer(opt *KafkaProducerOptions) *sarama.AsyncProducer {
+	config := opt.SaramaConfig
+	if config == nil {
+		config = sarama.NewConfig()
+	}
+	config.Producer.RequiredAcks = opt.Acks
+	config.Producer.Retry.Max = opt.RetryMax
+	config.Producer.Return.Successes = opt.ReturnSuccess
+	config.Producer.Compression = sarama.CompressionGZIP
+	config.Producer.Flush.Frequency = time.Duration(opt.FlushFrequency) * time.Millisecond
+
+	producer, err := sarama.NewAsyncProducer(opt.Brokers, config)
+	if err != nil {
+		log.Fatalf("failed to create kafka producer: %v", err)
+	}
+
+	return &producer
+}
+
+func NewKafkaConsumerGroup(opt *KafkaConsumerOptions) *sarama.ConsumerGroup {
+	config := opt.SaramaConfig
+	if config == nil {
+		config = sarama.NewConfig()
+	}
+	config.Consumer.Return.Errors = true
+	config.Consumer.Offsets.Initial = opt.InitialOffset
+
+	consumerGroup, err := sarama.NewConsumerGroup(opt.Brokers, opt.ConsumerGroup, config)
+	if err != nil {
+		log.Fatalf("failed to create kafka consumer group: %v", err)
+	}
+
+	return &consumerGroup
 }
---
+++ pkg/database/postgres.go
@@ -0,0 +1,45 @@
+package database
+
+import (
+	"fmt"
+	"log"
+	"time"
+
+	_ "github.com/jackc/pgx/v5/stdlib"
+	"github.com/jmoiron/sqlx"
+)
+
+type PostgresOptions struct {
+	Host            string
+	DbName          string
+	Username        string
+	Password        string
+	Sslmode         string
+	Port            int
+	MaxIdleConn     int
+	MaxOpenConn     int
+	MaxConnLifetime int
+}
+
+func NewPostgres(opt *PostgresOptions) *sqlx.DB {
+	dsn := fmt.Sprintf(
+		"host=%s user=%s password=%s dbname=%s port=%d sslmode=%s TimeZone=Asia/Tehran",
+		opt.Host,
+		opt.Username,
+		opt.Password,
+		opt.DbName,
+		opt.Port,
+		opt.Sslmode,
+	)
+
+	db, err := sqlx.Connect("pgx", dsn)
+	if err != nil {
+		log.Fatalf("error connecting to database: %v", err)
+	}
+
+	db.SetMaxIdleConns(opt.MaxIdleConn)
+	db.SetMaxOpenConns(opt.MaxOpenConn)
+	db.SetConnMaxLifetime(time.Duration(opt.MaxConnLifetime) * time.Minute)
+
+	return db
+}
