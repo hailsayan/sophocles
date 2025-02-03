bb6796be8fc7483190b449eb4d3bd1666f655daf
Psyon <psyon.dev@gmail.com>
2025-02-03 17:18:50 +0330

    feat: Add Redis database

---
+++ pkg/database/redis.go
@@ -0,0 +1,42 @@
+package database
+
+import (
+	"context"
+	"log"
+	"time"
+
+	"github.com/redis/go-redis/v9"
+)
+
+type RedisClusterOptions struct {
+	Addrs           []string
+	Password        string
+	DialTimeout     int
+	ReadTimeout     int
+	WriteTimeout    int
+	MaxIdleConn     int
+	MaxActiveConn   int
+	MaxConnLifetime int
+}
+
+func NewRedisCluster(opt *RedisClusterOptions) *redis.ClusterClient {
+	rdb := redis.NewClusterClient(&redis.ClusterOptions{
+		Addrs:           opt.Addrs,
+		Password:        opt.Password,
+		DialTimeout:     time.Duration(opt.DialTimeout) * time.Second,
+		ReadTimeout:     time.Duration(opt.ReadTimeout) * time.Second,
+		WriteTimeout:    time.Duration(opt.WriteTimeout) * time.Second,
+		MinIdleConns:    opt.MaxIdleConn,
+		ConnMaxLifetime: time.Duration(opt.MaxConnLifetime) * time.Minute,
+		MaxActiveConns:  opt.MaxActiveConn,
+	})
+
+	ctx := context.Background()
+	status, err := rdb.Ping(ctx).Result()
+	if err != nil {
+		log.Fatalf("failed to connect to redis cluster: %v", err)
+	}
+	log.Println("connected to redis cluster:", status)
+
+	return rdb
+}
